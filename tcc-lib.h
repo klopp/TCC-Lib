273d590a7cdf4326709f03a7e1908cb77c960463
klopp <klopp@yandex.ru>
2016-06-20 22:56:22 +0300

    created

---
+++ tcc-lib.h
@@ -0,0 +1,35 @@
+/*
+ *  Created on: 20 июня 2016 г.
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+/*
+ *  That's All, Folks!
+ */
+#ifndef TCC_LIB_H_
+#define TCC_LIB_H_
+
+#if defined(__cplusplus)
+  extern "C" {
+#endif
+
+#include "../tcc/tcc.h"
+#include "../tcc/libtcc.h"
+#include <limits.h>
+
+typedef struct _TccLib
+{
+        TCCState ts;
+} TccLib;
+
+TccLib *TccLibInit( TccLib *pc );
+void TccLibDown( TccLib *pc );
+TccLib *TccLibReset( TccLib *pc );
+
+
+#if defined(__cplusplus)
+  }; /* extern "C" */
+#endif
+
+
+#endif /* TCC_LIB_H_ */
